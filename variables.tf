60ef020fcd454db92fa0150d8a3ad175fa87cb1c
JohnKolapo <kollyp2000@outlook.com>
2025-04-29 22:38:35 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+variable "location" {
+  description = "Azure region for deployment"
+  type        = string
+  default     = "canadacentral"
+}
+
+variable "resource_group_name" {
+  description = "Azure Resource Group name"
+  type        = string
+  default     = "rg-bumima-dev"
+}
 
