5add3116a264791d768cadbbcf91165165f6af1d
JohnKolapo <kollyp2000@outlook.com>
2025-04-29 22:58:03 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,23 @@ variable "resource_group_name" {
   default     = "rg-bumima-dev"
 }
 
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
