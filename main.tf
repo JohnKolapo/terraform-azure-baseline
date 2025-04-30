604690dc3139f3598200090e7a2943c44debf6d1
JohnKolapo <kollyp2000@outlook.com>
2025-04-29 22:14:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
 module "vnet" {
   source              = "./modules/vnet"
   vnet_name           = "vnet-bumima-dev"
