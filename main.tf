7c4fb414a377126dbe65bc239558c2169c785af8
JohnKolapo <kollyp2000@outlook.com>
2025-04-29 20:43:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+module "vnet" {
+  source              = "./modules/vnet"
+  vnet_name           = "vnet-bumima-dev"
+  address_space       = "10.0.0.0/16"
+  subnet_name         = "subnet-core"
+  subnet_prefix       = "10.0.1.0/24"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
 
