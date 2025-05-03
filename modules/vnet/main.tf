db84a4d07fc286d3035ea44c558acb47b0e25284
JohnKolapo <kollyp2000@outlook.com>
2025-05-03 14:19:19 -0700

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
   address_space       = [var.address_space]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "this" {
