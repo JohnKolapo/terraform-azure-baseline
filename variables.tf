d460ffc3a1ce94ea966624319326d01a5e74df54
JohnKolapo <kollyp2000@outlook.com>
2025-05-03 15:47:32 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,23 @@ variable "location" {
   description = "Azure region to deploy resources into"
   type        = string
 }
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the VNet"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Name of the Subnet"
+  type        = string
+}
+
+variable "subnet_prefix" {
+  description = "Address prefix for the Subnet"
+  type        = list(string)
+}
