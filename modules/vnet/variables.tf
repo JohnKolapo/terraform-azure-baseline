f284bb9ead54dafa11f29f0c1d0f0cd4421c4935
JohnKolapo <kollyp2000@outlook.com>
2025-05-03 14:40:31 -0700

    Update variables.tf

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -4,7 +4,17 @@ variable "vnet_name" {
 }
 
 variable "address_space" {
-  description = "Address space for the Virtual Network"
+  description = "Address space for the VNet"
+  type        = list(string)
+}
+
+variable "location" {
+  description = "Azure location for the VNet"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
   type        = string
 }
 
@@ -15,16 +25,7 @@ variable "subnet_name" {
 
 variable "subnet_prefix" {
   description = "Address prefix for the Subnet"
-  type        = string
+  type        = list(string)
 }
 
-variable "location" {
-  description = "Azure location"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Azure Resource Group name"
-  type        = string
-}
 
