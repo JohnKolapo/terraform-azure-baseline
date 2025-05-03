17cf31d0ba21adf60163d7148c4617f7aebf6caf
JohnKolapo <kollyp2000@outlook.com>
2025-05-03 07:11:41 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,13 @@ variable "tenant_id" {
   type        = string
   description = "Azure AD tenant ID"
 }
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region to deploy resources into"
+  type        = string
+}
